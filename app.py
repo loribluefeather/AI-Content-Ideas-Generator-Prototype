a8b5f68417edc685708dffbf91dd734bf1b6b445
loribluefeather <lorihill63@gmail.com>
2024-09-20 17:26:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from serp import search_google_web_automation  # Custom function to automate Goo
 from my_functions import get_article_from_url, generate_ideas  # Custom functions for processing articles
 
 # Setting input parameters
-search_query = "AI in marketing"  # The query to search for
+search_query = "Tips to make money online"  # The query to search for
 NUMBER_OF_RESULTS = 10  # Number of search results to process
 
 # Template for the prompt to be used later
